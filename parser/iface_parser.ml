f6885f40fce22360fceb8011d32c61dde299db14
julbouln <jboulnois@immateriel.fr>
2005-02-14 11:25:40 +0000

    add simple iface xml parser

---
+++ parser/iface_parser.ml
@@ -0,0 +1,82 @@
+open Event;;
+
+open Video;;
+open Medias;;
+open Cursors;;
+
+open Main;;
+open Stage;;
+
+open Interface;;
+open Iface_event;;
+open Iface_xml;;
+
+open Iface_object;;
+open Iface_button;;
+
+
+(*
+<main>
+ <info>
+  <cmd str="iface_parser"/>
+  <name str="Interface parser"/>
+  <version str="0.1"/>  
+ <info>
+ <screen_size w="800" h="600"/>
+ <screen_default_size w="1024" h="768"/>
+<!-- <cursor path="medias/misc/cursor.png"/>  -->
+
+</main>
+
+*)
+
+let rec usleep sec = ignore (Unix.select [] [] [] sec);;
+
+main#info#set_cmd "iface_parser";
+main#info#set_name "Interface Parser";
+main#info#set_version "0.1";
+
+main#set_scr_w 800;
+main#set_scr_h 600;
+main#parse_args();
+main#medias_init();;
+
+video#set_def_size 800 600;;
+
+let curs=new cursors 30 30 None;;
+
+let stages=new stages curs;;
+
+class iface_parser=
+object (self)
+  inherit iface_stage curs Sys.argv.(1) as super
+
+  method on_loop()=
+    video#blank();
+    super#on_loop();
+
+(*    usleep(1./.30.); *)
+
+  method ev_parser e=
+    super#ev_parser e;
+    (match e with
+       | EventKeyboard ek ->
+	   (match ek with
+	      | KeyboardPress (k,uk)-> 
+		  (match k with 
+		     | KeyEchap -> 
+			 exit 2;
+		     | KeyReturn ->
+			 ()
+		     | _ ->()
+		  )
+	      | _ -> ()
+	   )
+       | _ -> ()
+    )
+
+end;;
+
+
+stages#stage_add "parser" (new iface_parser:>stage);
+stages#stage_load "parser"; 
